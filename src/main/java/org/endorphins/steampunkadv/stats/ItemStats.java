4a6976f66ca52a55bf8b39292854e07604589e73
Tyler Pacheco <tyler.n.pacheco@gmail.com>
2015-07-06 14:49:35 -0700

    Update ItemStats.java
    Removed toString(), added getStatSheet(), added curr/max durability floats

--- src/main/java/org/endorphins/steampunkadv/stats/ItemStats.java
+++ src/main/java/org/endorphins/steampunkadv/stats/ItemStats.java
@@ -2,9 +2,13 @@ package org.endorphins.steampunkadv.stats;
 
 public class ItemStats extends Stats{
     
-    @Override
-    public String toString()
-    {
-        return "!!!THIS SHOULD BE THE ITEMSTATS!!!";
+    public float currDurability = 100f;
+    public float maxDurability = 100f;
+    
+    public String getStatSheet(){
+        return
+            "*------------------------------------------------*" + SteampunkAdvConstants.NEW_LINE +
+            "* Durability: " + currDurability + " / " + maxDurability + SteampunkAdvConstants.NEW_LINE +
+            super.getStatSheet();
     }
 }
