90a5eb4a9d6a02296f3eb8f94176fc58b158ce5f
Tyler Pacheco <tyler.n.pacheco@gmail.com>
2015-07-06 15:38:59 -0700

    Created GameState.java
    This will be the class used for saving the game. Will work by serializing the GameState (saving) and then unserializing it (load). Could also be used for autosaves if those get added.

---
+++ src/main/java/org/endorphins/steampunkadv/GameState.java
@@ -0,0 +1,13 @@
+package org.endorphins.steampunkadv;
+
+public class GameState implements Serializable{
+    private static final long serialVersionUID = 1l;
+    
+    //TODO
+    /* Things to have in GameState
+     * MainGame
+     * PlayerCharacter
+     * All NPCs
+     * Location
+     */
+}
